829f9af096442076f2dcf4116fac15cdff656e79
user <volodimirkomarnickij28@gmail.com>
2023-09-29 17:12:19 +0300

    Implemented basic models for post and user. Registered them in admin.py

--- post/models.py
+++ post/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from user.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name="liked_posts")
+
+    def __str__(self):
+        return f"Post by {self.user}"
--- social_network/settings.py
+++ social_network/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    "rest_framework",
+
+    "post",
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -99,6 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "user.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
